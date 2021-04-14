5bc855e56144e7485cbea27dc1bfff9063f46535
Jay-Chheda <jaychheda97@gmail.com>
2021-04-14 20:30:28 +0530

    add modal and book array

--- script.js
+++ script.js
@@ -1,61 +1,18 @@
-const bookArray = [];
-const modalButton = document.getElementById("modal-open");
-const modal = document.getElementById("my-modal");
-const bookList = document.getElementById("book-list");
+let myLibrary = [];
 
-const bookForm = document.getElementById("book-form");
 
-modalButton.addEventListener('click',(e)=>{
-      modal.style.display = "block";
+function Book(bookName,bookAuthor,bookPages,bookStatus){
+    this.bookName = bookName;
+    this.bookAuthor = bookAuthor;
+    this.bookPages = bookPages;
+    this.bookStatus = bookStatus;
 
-  
+}
 
-});
 
-window.addEventListener('click',(e)=>{
-    if(e.target === modal){
-       
-        modal.style.display = "none";
-    }
-});
-
-bookForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let formData = new FormData(bookForm);
-    const bookName = formData.get('book-name');
-    const bookAutor = formData.get('book-author');
-    const bookPages = formData.get('book-pages');
-    let bookStatus =formData.get('book-status');
-    if(bookStatus){
-        bookStatus = true;
-    }
-    else{
-        bookStatus = false;
-    }
-    const newBook = {
-        bookName,
-        bookAutor,
-        bookPages,
-        bookStatus
-    }
-    console.log(newBook);
-    bookArray.push(newBook);
-    e.target.reset();
-    modal.click();
-    displayBooks();
-
+function addBookToLibrary(name,author,pages,status){
+    const book = new Book(name,author,pages,status);
+    myLibrary.push(book);
     
- 
-})
 
-
-const displayBooks = ()=>{
-    bookArray.forEach(book=>{
-        const bookName = book['bookName'];
-        const newBook = document.createElement('li');
-        newBook.innerText = `${bookName}`;
-        bookList.appendChild(newBook);
-
-        
-    })
 }
