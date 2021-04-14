10b60a70bb41002e000d424bbb3ac773029cf119
Jay-Chheda <jaychheda97@gmail.com>
2021-04-14 21:40:54 +0530

    add new book form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// an array to store the books objects
 let myLibrary = [];
 
-
+// book constructor
 function Book(bookName,bookAuthor,bookPages,bookStatus){
     this.bookName = bookName;
     this.bookAuthor = bookAuthor;
@@ -9,7 +10,7 @@ function Book(bookName,bookAuthor,bookPages,bookStatus){
 
 }
 
-
+//function to add book to array
 function addBookToLibrary(name,author,pages,status){
     const book = new Book(name,author,pages,status);
     myLibrary.push(book);
