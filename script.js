1bc9f939a111a28a4b7a53b10e24a03091a327ef
Jay-Chheda <jaychheda97@gmail.com>
2021-04-16 20:28:25 +0530

    add method to populate display

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // an array to store the books objects
-let myLibrary = [];
+let myLibrary = [
+    {
+    bookName: "game of thornes",
+    bookAuthor: "grrm",
+    bookPages: "42454",
+    bookStatus: true
+    },
+
+    {
+        bookName: "2hornes",
+        bookAuthor: "grrm",
+        bookPages: "42454",
+        bookStatus: true
+        }
+    ];
+
 const newBookForm = document.getElementById("add-bk-form");
 const modalCloseBtn = document.getElementById("modal-close");
 
@@ -17,15 +32,17 @@ newBookForm.addEventListener("submit",(e)=>{
     let form = e.target;
     let formData = new FormData(form);
     let bookName = formData.get('book-name');
-    console.log(bookName);
+    
     let bookAuthor = formData.get('book-author');
-    console.log(bookAuthor);
+  
     let bookPages = formData.get('book-pages');
-    console.log(bookPages);
+   
     let bookStatus = formData.get('book-status');
     bookStatus = bookStatus? true:false;
     console.log(bookStatus);
+    
     addBookToLibrary(bookName,bookAuthor,bookPages,bookStatus);
+
 
 
 
@@ -41,7 +58,43 @@ newBookForm.addEventListener("submit",(e)=>{
 function addBookToLibrary(name,author,pages,status){
     const book = new Book(name,author,pages,status);
     myLibrary.push(book);
-    console.log(myLibrary);
-    
+    // console.log(myLibrary);
+    console.log(myLibrary[myLibrary.length-1]);
+    addBookToDom(myLibrary[myLibrary.length-1]);
 
+    }
+
+
+const bookShelf = document.getElementById("book-shelf");
+function addBookToDom(bookElement){
+    const bookName = bookElement["bookName"];
+    const bookAuthor = bookElement["bookAuthor"];
+    const bookPages = bookElement["bookPages"];
+    const bookStatus = bookElement["bookStatus"];
+
+    const colDiv = document.createElement("div");
+    colDiv.classList.add("col-lg-3", "col-md-6" ,"col-sm-12");
+    const bookInfoDiv = document.createElement("div");
+    bookInfoDiv.classList.add("book-info", "my-2" ,"py-3","border-blk");
+    const bookNameDiv  = document.createElement("div");
+    bookNameDiv.innerText = `${bookName}`;
+    const bookAuthorDiv  = document.createElement("div");
+    bookAuthorDiv.innerText = `${bookAuthor}`;
+    const bookPagesDiv  = document.createElement("div");
+    bookPagesDiv.innerText = `${bookPages}`;
+    const bookStatusDiv  = document.createElement("div");
+    bookStatusDiv.innerText = `${bookStatus}`;
+    bookInfoDiv.append(bookNameDiv,bookAuthorDiv,bookPagesDiv,bookStatusDiv);
+    colDiv.appendChild(bookInfoDiv);
+    bookShelf.appendChild(colDiv);
 }
+
+
+function reloadDisplay(){
+    myLibrary.forEach(element => {
+        addBookToDom(element);
+        
+    });
+}
+
+reloadDisplay();
