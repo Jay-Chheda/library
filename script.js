65248e6d597f30eaf2869f941bd4ef4fca7305e5
Jay-Chheda <jaychheda97@gmail.com>
2021-04-16 18:39:41 +0530

    add grid for book shelf

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // an array to store the books objects
 let myLibrary = [];
+const newBookForm = document.getElementById("add-bk-form");
+const modalCloseBtn = document.getElementById("modal-close");
 
 // book constructor
 function Book(bookName,bookAuthor,bookPages,bookStatus){
@@ -10,10 +12,36 @@ function Book(bookName,bookAuthor,bookPages,bookStatus){
 
 }
 
+newBookForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let form = e.target;
+    let formData = new FormData(form);
+    let bookName = formData.get('book-name');
+    console.log(bookName);
+    let bookAuthor = formData.get('book-author');
+    console.log(bookAuthor);
+    let bookPages = formData.get('book-pages');
+    console.log(bookPages);
+    let bookStatus = formData.get('book-status');
+    bookStatus = bookStatus? true:false;
+    console.log(bookStatus);
+    addBookToLibrary(bookName,bookAuthor,bookPages,bookStatus);
+
+
+
+  
+    e.target.reset();
+    modalCloseBtn.click();
+
+})
+
+
+
 //function to add book to array
 function addBookToLibrary(name,author,pages,status){
     const book = new Book(name,author,pages,status);
     myLibrary.push(book);
+    console.log(myLibrary);
     
 
 }
